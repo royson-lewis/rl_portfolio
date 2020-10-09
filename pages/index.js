afaefcb814ed3b0b14df87faa3e9c9a947715b38
unknown <lewisroysonj@gmail.com>
2020-10-10 03:56:30 +0530

    Initialize react folder structure

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+/** @format */
+
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Portfolio</h1>
+    </>
+  );
+}
