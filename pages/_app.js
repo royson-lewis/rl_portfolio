53c422bd680d48a54c9915ae5947313d24b1cb5a
unknown <lewisroysonj@gmail.com>
2020-10-17 04:42:40 +0530

    customize default styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+/** @format */
+
+import "../styles/globals.scss";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link href='https://fonts.googleapis.com/css2?family=Spartan:wght@400;500;600;750;800&display=swap' rel='stylesheet'></link>
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,55 @@
 /** @format */
 
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
     <>
-      <h1>Portfolio</h1>
+      <main>
+        <h1>Royson Lewis Portfolio</h1>
+        <h2>Royson Lewis Portfolio</h2>
+        <h3>Royson Lewis Portfolio</h3>
+        <h4>Royson Lewis Portfolio</h4>
+        <h5>Royson Lewis Portfolio</h5>
+        <h6>Royson Lewis Portfolio</h6>
+        <p>Paragraph text</p>
+        <a href='#'>A Link to a page</a>
+        <br></br>
+        <br></br>
+        <br></br>
+        <div>
+          <button>Submit</button>
+          <button disabled='disabled'>Submit</button>
+          <br></br>
+          <br></br>
+          <button>
+            <div className='spinnerHolderSmall'>
+              <div className='spinner'></div>
+            </div>
+          </button>
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <input></input>
+        <input disabled='disabled'></input>
+        <br></br>
+        <textarea className='inputSuccess'></textarea>
+        <ul>
+          <li>UnOrdered List</li>
+        </ul>
+        <ol>
+          <li>Ordered List</li>
+        </ol>
+        {/* <div className='progress'>
+          <div className='bar'></div>
+        </div>
+        <div className='spinnerHolderFull'>
+          <div className='spinner'></div>
+        </div> */}
+      </main>
     </>
   );
 }
