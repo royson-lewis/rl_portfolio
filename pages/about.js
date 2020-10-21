cd2bc7f0d93953248b8d3b4d9abc8eae2a5e55f2
unknown <lewisroysonj@gmail.com>
2020-10-21 23:59:00 +0530

    build about page

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,21 @@ import styles from "../styles/about.module.scss";
 export default function About() {
   return (
     <main className={styles.about}>
-      <h2>About</h2>
+      <h2>About Me</h2>
+      <svg xmlns='http://www.w3.org/2000/svg' width='167.879' height='167.885' viewBox='0 0 167.879 167.885'>
+        <g id='Group_205' data-name='Group 205' transform='translate(0)'>
+          <path id='Path_238' data-name='Path 238' d='M429.879,195.939a83.654,83.654,0,0,1-6.76,33.054l-.278.641a84.078,84.078,0,0,1-91.466,48.985q-.756-.134-1.51-.278a83.188,83.188,0,0,1-15.184-4.478q-1.2-.481-2.374-1c-.536-.233-1.073-.474-1.6-.723a83.947,83.947,0,1,1,119.176-76.206Z' transform='translate(-262 -112)' fill='#dbdbdb' />
+          <path id='Path_241' data-name='Path 241' d='M482.467,307.094a33.191,33.191,0,0,0,1.913,17.034,30.248,30.248,0,0,0,5.738-32.373A33.193,33.193,0,0,0,482.467,307.094Z' transform='translate(-427.407 -247.115)' opacity='0.2' />
+          <circle id='Ellipse_85' data-name='Ellipse 85' cx='32.724' cy='32.724' r='32.724' transform='translate(51.043 36.482)' fill='#f8d1a5' />
+          <path id='Path_244' data-name='Path 244' d='M547.833,463.013s4.09,19.43,4.09,21.475,19.43,11.249,19.43,11.249l17.385-3.068,6.136-18.407s-10.226-15.339-10.226-21.475Z' transform='translate(-476.849 -368.753)' fill='#f8d1a5' />
+          <path id='Path_245' data-name='Path 245' d='M517.256,533.626l-.278.641a84.078,84.078,0,0,1-91.466,48.985q-.756-.134-1.51-.278a83.2,83.2,0,0,1-15.184-4.478q-1.2-.481-2.374-1c-.536-.233-1.073-.474-1.6-.723a83.835,83.835,0,0,1-13.721-8.016l-3.879-11.252,2.18-1.664,4.485-3.425,4.786-3.653,1.244-.951,18.777-14.334,1.386-1.058,9.76-7.45,0,0s10.554,17.2,25.895,12.084a21.153,21.153,0,0,0,15-19.775Z' transform='translate(-356.138 -417.23)' fill='#3f3d56' />
+          <path id='Path_246' data-name='Path 246' d='M461.741,232.149s10.39-22.511,30.3-17.316,31.169,12.987,32.035,20.779-.433,19.481-.433,19.481-2.165-16.017-16.017-12.554-35.5.866-35.5.866l-3.463,31.169s-3.9-5.628-8.225-2.165S447.888,239.076,461.741,232.149Z' transform='translate(-406.352 -189.308)' fill='#2f2e41' />
+          <path id='Path_247' data-name='Path 247' d='M396.251,700.257q-1.2-.48-2.374-1c-.536-.234-1.073-.474-1.6-.723a84.145,84.145,0,0,1-21.752-14.578,29.435,29.435,0,0,1,4.152-4.691h15.34l2.3,7.733Z' transform='translate(-343.57 -538.991)' fill='#3f3d56' />
+          <path id='Path_248' data-name='Path 248' d='M853.836,576.32,867,578.644a83.923,83.923,0,0,1-14.1,22Z' transform='translate(-706.156 -461.608)' fill='#3f3d56' />
+        </g>
+      </svg>
+      <h3>Hi, I`m Royson Lewis ❤</h3>
+      <p>I‘m a UX/UI Designer with six years experience conceptualizing and crafting digital products, helping businesses and non-profits expand their capacity for impact. Working in product strategy, facilitation and prototyping with a solid background in interface design and front-end development.</p>
     </main>
   );
 }
