faaba4a6784040a8904ed8228f3e8a51af70d8f0
Royson Lewis <royson.lewis@aitrade.ai>
2022-11-24 23:36:43 +0530

    35 build hero section

--- src/view/screens/home/main.tsx
+++ src/view/screens/home/main.tsx
@@ -1,27 +1,42 @@
-import Head from 'next/head'
-
 import styles from './main.module.scss'
+import Image from 'next/image'
+import HeroEllipse from './assets/hero-ellipse.svg'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGithub, faLinkedin, faDribbble} from "@fortawesome/free-brands-svg-icons";
+import React from "react";
+import ButtonMain from "../../../components/button/main";
 
 const HomeMain = () => (
   <>
-    <Head>
-      <title>Home - Royson Lewis</title>
-      <meta
-        name="description"
-        content="Welcome to my Portfolio!, Explore some of my best projects, Services and Skills showcased in this portfolio. Contact me for any queries through the various options available in the contact section."
-      />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content="Home - Royson Lewis" />
-      <meta
-        property="og:description"
-        content="Welcome to my Portfolio!, Explore some of my best projects, Services and Skills showcased in this portfolio. Contact me for any queries through the various options available in the contact section."
-      />
-      <meta property="og:url" content="http://www.roysonlewis.com/" />
-      <meta property="og:site_name" content="Royson Lewis" />
-      <meta property="og:image" content="/royson cover@2x.png" />
-    </Head>
-    <main className={styles.homePage}>Homepage</main>
+    <main className={styles['home-page']}>
+      <HeroSection />
+    </main>
   </>
 )
+
+const HeroSection = () => {
+  return (
+    <section className={styles["hero-section"]}>
+      <Image width='100' height="100" className={styles['hero-ellipse']} src={HeroEllipse.src} alt="Hero ellipse" />
+      <div className={styles['hero-info']}>
+        <h2>Hi, I'm <strong>Royson Lewis</strong></h2>
+        <h3>UI Designer<span>,</span></h3>
+        <h3>Software Developer</h3>
+        <div className={styles['social-container']}>
+          <span className={styles['icon-wrapper']}>
+            <FontAwesomeIcon icon={faGithub} />
+          </span>
+          <span className={styles['icon-wrapper']}>
+            <FontAwesomeIcon icon={faLinkedin} />
+          </span>
+          <span className={styles['icon-wrapper']}>
+            <FontAwesomeIcon icon={faDribbble} />
+          </span>
+        </div>
+        <ButtonMain className="no-border">View Resume</ButtonMain>
+      </div>
+    </section>
+  )
+}
 
 export default HomeMain
