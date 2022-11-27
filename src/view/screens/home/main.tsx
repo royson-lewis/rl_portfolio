41f70c8d896922f3bdf300c3dedb458c0171b3df
Royson Lewis <royson.lewis@aitrade.ai>
2022-11-27 20:13:44 +0530

    35 skills section

--- src/view/screens/home/main.tsx
+++ src/view/screens/home/main.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import styles from './main.module.scss'
 import SectionHeroMain from "./section/hero/main";
 import SectionProjectsMain from "./section/projects/main";
+import SectionSkillsMain from "./section/skills/main";
 
 const HomeMain = () => (
   <>
     <main className={styles['home-page']}>
       <SectionHeroMain />
       <SectionProjectsMain />
+      <SectionSkillsMain />
     </main>
   </>
 )
