fad28180ded7a7b35dcb7811095c1a32c92ccc0d
Royson Lewis <royson.lewis@aitrade.ai>
2022-11-27 16:31:44 +0530

    35 hero section responsiveness

--- src/view/layout/main/index.tsx
+++ src/view/layout/main/index.tsx
@@ -9,7 +9,7 @@ const LayoutMain: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <>
       <HeaderMain isDesktop={!!windowSize?.width && windowSize?.width > 768} />
-      <main style={{ margin: '20px 30px', minHeight: 'calc(100vh - 68px)' }}>{children}</main>
+      <main>{children}</main>
       <FooterMain />
     </>
   )
--- src/view/screens/home/main.tsx
+++ src/view/screens/home/main.tsx
@@ -1,42 +1,13 @@
-import styles from './main.module.scss'
-import Image from 'next/image'
-import HeroEllipse from './assets/hero-ellipse.svg'
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faGithub, faLinkedin, faDribbble} from "@fortawesome/free-brands-svg-icons";
 import React from "react";
-import ButtonMain from "../../../components/button/main";
+import styles from './main.module.scss'
+import SectionHeroMain from "./section/hero/main";
 
 const HomeMain = () => (
   <>
     <main className={styles['home-page']}>
-      <HeroSection />
+      <SectionHeroMain />
     </main>
   </>
 )
-
-const HeroSection = () => {
-  return (
-    <section className={styles["hero-section"]}>
-      <Image width='100' height="100" className={styles['hero-ellipse']} src={HeroEllipse.src} alt="Hero ellipse" />
-      <div className={styles['hero-info']}>
-        <h2>Hi, I'm <strong>Royson Lewis</strong></h2>
-        <h3>UI Designer<span>,</span></h3>
-        <h3>Software Developer</h3>
-        <div className={styles['social-container']}>
-          <span className={styles['icon-wrapper']}>
-            <FontAwesomeIcon icon={faGithub} />
-          </span>
-          <span className={styles['icon-wrapper']}>
-            <FontAwesomeIcon icon={faLinkedin} />
-          </span>
-          <span className={styles['icon-wrapper']}>
-            <FontAwesomeIcon icon={faDribbble} />
-          </span>
-        </div>
-        <ButtonMain className="no-border">View Resume</ButtonMain>
-      </div>
-    </section>
-  )
-}
 
 export default HomeMain
