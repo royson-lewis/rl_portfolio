00fd24aada93386fa2538feb85ca69c18d74fd66
Royson Lewis <royson.lewis@aitrade.ai>
2022-11-28 15:07:48 +0530

    37 project info section

--- src/view/screens/projects/project/details.tsx
+++ src/view/screens/projects/project/details.tsx
@@ -2,11 +2,18 @@ import React from 'react'
 
 import styles from './details.module.scss'
 import SectionHeroMain from "./section/hero/main";
+import Image from "next/image";
+import EssenceMainImage from '../assets/essence-main-image.png'
+import SectionProjectInfoMain from "./section/project-info/main";
 
 const ProjectDetails = () => (
   <>
     <main className={styles['project-details-page']}>
       <SectionHeroMain />
+      <section className={styles['main-image']}>
+        <Image src={EssenceMainImage} alt="project presentation in different devices" />
+      </section>
+      <SectionProjectInfoMain />
     </main>
   </>
 )
